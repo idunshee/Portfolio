110f3fd36b5161686a51eb7e9994ef67cbc1a68c
idunshee <idunshee@uiowa.edu>
2016-05-22 14:56:50 -0500

    added new pointcloud

---
+++ Project/WPC/WPC_Viewer/testian1cm.pts_converted/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 3339412,
+    "boundingBox": {
+        "lx": -2.847122,
+        "ly": -0.280136,
+        "lz": -1.560928,
+        "ux": 5.8887789999999999,
+        "uy": 8.455765,
+        "uz": 7.174973
+    },
+    "tightBoundingBox": {
+        "lx": -2.847122,
+        "ly": -0.280136,
+        "lz": -1.560928,
+        "ux": 5.888779,
+        "uy": 7.581894,
+        "uz": 0.649673
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.07565512508153916,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
